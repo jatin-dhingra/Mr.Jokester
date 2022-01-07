20d563508299cfa154841549b974e2cd622ee5bc
Jatin Dhingra <68529999+jatin-dhingra@users.noreply.github.com>
2022-01-07 10:56:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const bodyParser = require("body-parser");
+
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/proindex.html");
+});
+
+app.listen(3000,()=>{
+    console.log('listening on port 3000');
+})
